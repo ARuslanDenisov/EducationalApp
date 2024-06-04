c248e83f3fe9195b83f1b0d68661437e16ea5523
ARuslanDenisov <angess47@gmail.com>
2024-06-04 22:31:55 +0700

    Initial Commit

---
+++ EducationalApp/EducationalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EducationalAppApp.swift
+//  EducationalApp
+//
+//  Created by Руслан on 04.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct EducationalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
