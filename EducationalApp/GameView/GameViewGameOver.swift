c0aafc6ad8c70f80ffb55a7d66efe0d4213f3248
ARuslanDenisov <angess47@gmail.com>
2024-06-05 22:41:53 +0700

    continue 2

---
+++ EducationalApp/GameView/GameViewGameOver.swift
@@ -0,0 +1,18 @@
+//
+//  GameViewGameOver.swift
+//  EducationalApp
+//
+//  Created by Руслан on 05.06.2024.
+//
+
+import SwiftUI
+
+struct GameViewGameOver: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameViewGameOver()
+}
